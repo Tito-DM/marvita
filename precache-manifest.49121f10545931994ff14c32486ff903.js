0c992973c07453c14addb959cadeb0f881161fe5
pyong <muandatito@gmail.com>
2020-12-27 22:03:27 +0000

    Updates

---
+++ precache-manifest.49121f10545931994ff14c32486ff903.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c5cee4105cd497cd5a2ae1f65aecb1",
+    "url": "/marvita/index.html"
+  },
+  {
+    "revision": "1865ec86a43de6499c25",
+    "url": "/marvita/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "122058f4132895d8f156",
+    "url": "/marvita/static/css/main.284e1ccc.chunk.css"
+  },
+  {
+    "revision": "1865ec86a43de6499c25",
+    "url": "/marvita/static/js/2.430b68e6.chunk.js"
+  },
+  {
+    "revision": "4704814cfbfc26d598a03749ded69371",
+    "url": "/marvita/static/js/2.430b68e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "122058f4132895d8f156",
+    "url": "/marvita/static/js/main.3aaf5b71.chunk.js"
+  },
+  {
+    "revision": "f88e9ef5383c554b826b",
+    "url": "/marvita/static/js/runtime-main.499ca5ae.js"
+  },
+  {
+    "revision": "9313969501be17c9e5c2ca51d2f6ebc8",
+    "url": "/marvita/static/media/House1.93139695.jpg"
+  },
+  {
+    "revision": "06553c492df96df28fbe21d74bf02af7",
+    "url": "/marvita/static/media/imgbckg.06553c49.png"
+  },
+  {
+    "revision": "46a3b4e36d4baaffe10f836b465acf74",
+    "url": "/marvita/static/media/pic.46a3b4e3.jpg"
+  }
+]);
